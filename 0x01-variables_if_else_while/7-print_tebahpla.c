1fa4671cbdeaf9db90d50f7fcac13f37fc426df8
Cathia-abi <cathia.abi@gmail.com>
2022-03-18 11:01:06 +0000

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,8 +9,8 @@ int main(void)
 	char alphabet;
 
 	for (alphabet = 122; alphabet >= 97; alphabet++)
-		putchar (alphabet);
-	putchar ('\n');
+		putchar(alphabet);
+	putchar('\n');
 	return (0);
 }
 
