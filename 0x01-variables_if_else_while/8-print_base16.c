436605a512696807bd48942dfe3671183bd40f8b
Cathia-abi <100198770+Cathia-abi@users.noreply.github.com>
2022-03-23 00:24:39 +0000

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
+
 /**
- * main -Entry Point
- * Return: Always 0 (success/correst)
- *
- */
+  * main - Prints a hexadecimal string
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
-	char base_16;
+	char c;
 
-	for (base_16 = 48; base_16 <= 57; base_16++)
-		putchar (base_16);
-	for (base_16 = 97; base_16 <= 102; base_16++)
-		putchar (base_16);
-	putchar ('\n');
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
 	return (0);
 }
